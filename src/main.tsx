4010f0ea5d7cdb9d33aed41e034ca761d6c6ade2
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-07 21:51:40 -0500

    Update: Added auth provider to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { MoviesContextProvider } from "./context/Movie.tsx";
+import { AuthProvider } from "./context/Auth.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MoviesContextProvider>
-      <App />
-    </MoviesContextProvider>
+    <AuthProvider>
+      <MoviesContextProvider>
+        <App />
+      </MoviesContextProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
