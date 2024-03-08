6773273deb40b83939ef75a30cbd6afe5cc9113c
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 14:32:48 -0500

    Update: Removed old search input and changed for nav item

--- src/components/pages/home/GoToSearchBanner.tsx
+++ src/components/pages/home/GoToSearchBanner.tsx
@@ -1,27 +1,13 @@
-import { SVG_ICONS } from "../../../helpers/svgIcons";
 import "../../../styles/pages/home/GoToSearchBanner.css";
-import SimpleButton from "../../shared/SimpleButton";
+import NavItem from "../../layout/Navbar/NavItem";
+import { SVG_ICONS } from "../../../helpers/svgIcons";
 
 const GoToSearchBanner = () => {
   return (
     <div className="go-to-search-banner flex centered">
       <p>Don't find what you're looking for?</p>
       <p>Search for it</p>
-      <form className="flex centered" style={{ gap: "10px" }}>
-        <label aria-label="hidden" htmlFor="search" />
-        <input
-          name="search"
-          id="search"
-          className="searchbar"
-          type="search"
-          placeholder="Type the name"
-        />
-        <SimpleButton
-          showTitle={false}
-          title="Search"
-          icon={SVG_ICONS.SEARCH}
-        />
-      </form>
+      <NavItem to="search" title="GO TO SEARCH" icon={SVG_ICONS.SEARCH} />
     </div>
   );
 };
