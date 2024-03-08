d24fde18e1e5bc987577f89063183d9414c3d122
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 15:53:12 -0500

    Feat: Implemented no movie img

--- src/components/MoviesCard/MovieImage.tsx
+++ src/components/MoviesCard/MovieImage.tsx
@@ -1,12 +1,12 @@
 import "../../styles/components/MoviesCard/MovieImage.css";
-
+import NoImage from "../../assets/no-img.jpg";
 const IMAGE_SOURCE: string = "https://image.tmdb.org/t/p/w500/";
 
 const MovieImage = (props: { source: string; alt: string }) => {
   return (
     <img
       className="card-image"
-      src={`${IMAGE_SOURCE}${props.source}`}
+      src={props.source ? `${IMAGE_SOURCE}${props.source}` : NoImage}
       alt={props.alt}
       title={`${props.alt} poster`}
       loading="lazy"
