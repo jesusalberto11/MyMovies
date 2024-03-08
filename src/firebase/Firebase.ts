c70749939ddc3be5c6536304db532a8bc7919c47
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 14:28:10 -0500

    Update: Replaced API keys with .env values

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCx3We_0EwwZEJ4hP73yV9A9O8xzjLwlu4",
-  authDomain: "my-movies-login.firebaseapp.com",
-  projectId: "my-movies-login",
-  storageBucket: "my-movies-login.appspot.com",
-  messagingSenderId: "793715895477",
-  appId: "1:793715895477:web:5747ecb99ba1bcefc1e2fc",
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
