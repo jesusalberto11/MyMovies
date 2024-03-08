b1df63c7366a2c285ace6f5f31d2a09e6de40370
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 09:38:21 -0500

    Feat: Added not found page route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
