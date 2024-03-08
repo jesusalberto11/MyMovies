8d8143e85e650d9fa9ca820b43df29048e83e87f
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 13:39:07 -0500

    Feat: Added go to register page button

--- src/components/pages/Login/LoginForm.tsx
+++ src/components/pages/Login/LoginForm.tsx
@@ -2,59 +2,67 @@ import { useState } from "react";
 import "../../../styles/components/pages/login/LoginForm.css";
 import ActionButton from "../../shared/ActionButton";
 import { SVG_ICONS } from "../../../helpers/svgIcons";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = (props: { onSubmitForm: any; onGoogleSignIn: Function }) => {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <>
-      <form
-        className="form-container flex centered"
-        onSubmit={(e) => props.onSubmitForm(e, email, password)}
-      >
-        <h1 className="pacifico-font">Sign In</h1>
-        <div className="form-item flex">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            className="login-input"
-            placeholder="Email"
-            type="email"
-            autoComplete="email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-item flex">
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            className="login-input"
-            type="password"
-            placeholder="Password"
-            autoComplete="current-password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <ActionButton
-          title="Sign In"
-          icon={SVG_ICONS.LOG_IN}
-          type="submit"
-          onClickButton={() => {}}
+    <form
+      className="form-container flex centered"
+      onSubmit={(e) => props.onSubmitForm(e, email, password)}
+    >
+      <h1 className="pacifico-font">Sign In</h1>
+      <div className="form-item flex">
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          className="login-input"
+          placeholder="Email"
+          type="email"
+          autoComplete="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-
-        <ActionButton
-          title="Enter with Google"
-          icon={SVG_ICONS.GOOGLE}
-          type="button"
-          onClickButton={props.onGoogleSignIn}
+      </div>
+      <div className="form-item flex">
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          className="login-input"
+          type="password"
+          placeholder="Password"
+          autoComplete="current-password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-      </form>
-    </>
+      </div>
+      <ActionButton
+        title="Sign In"
+        icon={SVG_ICONS.LOG_IN}
+        type="submit"
+        onClickButton={() => {}}
+      />
+      <ActionButton
+        title="Create accout"
+        icon={SVG_ICONS.CREATE_ACCOUT}
+        type="button"
+        onClickButton={() => {
+          navigate("../register", { replace: true });
+        }}
+      />
+      <ActionButton
+        title="Enter with Google"
+        icon={SVG_ICONS.GOOGLE}
+        type="button"
+        onClickButton={props.onGoogleSignIn}
+      />
+    </form>
   );
 };
 
