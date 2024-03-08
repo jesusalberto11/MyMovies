90af22db90224b89de9a43070902a9db72438499
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 12:35:08 -0500

    Feat: NavItems now are React router link components

--- src/components/layout/Navbar/NavItem.tsx
+++ src/components/layout/Navbar/NavItem.tsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import "../../../styles/components/layout/Navbar/NavItem.css";
 
-const NavItem = (props: { title: string; icon: string }) => {
+const NavItem = (props: { title: string; icon: string; to: string }) => {
   return (
-    <li className="nav-item centered">
+    <Link className="nav-item centered" to={props.to}>
       <svg width="16" height="16" fill="#FFF" viewBox="0 0 16 16">
         <path d={props.icon} />
       </svg>
-      <a>{props.title}</a>
-    </li>
+      {props.title}
+    </Link>
   );
 };
 
