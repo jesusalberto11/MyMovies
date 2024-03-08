24c16f53e10db3e9be9ba2407da022e2a9cf5957
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 11:56:21 -0500

    Feat: Created login form component

---
+++ src/components/pages/Login/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "../../../styles/components/pages/login/LoginForm.css";
+import ActionButton from "../../shared/ActionButton";
+import { SVG_ICONS } from "../../../helpers/svgIcons";
+
+const LoginForm = (props: { onSubmitForm: any; onGoogleSignIn: Function }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <form
+        className="form-container flex centered"
+        onSubmit={(e) => props.onSubmitForm(e, email, password)}
+      >
+        <h1 className="pacifico-font">Sign In</h1>
+        <div className="form-item flex">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            className="login-input"
+            placeholder="Email"
+            type="email"
+            autoComplete="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-item flex">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            className="login-input"
+            type="password"
+            placeholder="Password"
+            autoComplete="current-password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <ActionButton
+          title="Sign In"
+          icon={SVG_ICONS.LOG_IN}
+          onClickButton={(e: any) => props.onSubmitForm(e, email, password)}
+        />
+
+        <ActionButton
+          title="Enter with Google"
+          icon={SVG_ICONS.GOOGLE}
+          onClickButton={props.onGoogleSignIn}
+        />
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
