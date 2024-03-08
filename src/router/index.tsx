4d56c6ea14b401a9010cf95dbe471925eb282fed
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-08 12:44:15 -0500

    Feat: Added register page route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
+import Register from "../pages/Register";
 
 export const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
       {
         path: "login",
         element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
       },
     ],
   },
